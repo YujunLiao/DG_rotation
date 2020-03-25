00d3fbc446199cd27e8965b026ef21058cecd1e0
YujunLiao <18916108830@163.com>
2020-03-25 23:40:02 +0800

    Update code to read txt to xlsx.

--- scripts/read_file.py
+++ scripts/read_file.py
@@ -8,29 +8,39 @@ wb = Workbook()
 # grab the active worksheet
 ws = wb.active
 
-# path = "/home/lyj//Files/project/pycharm/DG_rotation/trainer_utils/output_manager/output_file/"
-file_name = 'DA_rotation/resnet50/0.6_0.6_0.7_0.1/'
-path = "/home/lyj/Files/project/pycharm/DG_rotation/trainer_utils/output_manager/output_file/era/"+ file_name
+path = '/home/lyj/Files/project/pycharm/DG_rotation/trainer_utils/output_manager/output_file/'\
++'era/DA_rotation/resnet50/'
+path_of_different_parameter_pairs = os.listdir(path)
+path_of_different_parameter_pairs.sort()
 
-files= os.listdir(path)
-files.sort()
-# print(files)
-column=0
-# files = ['cartoon_art_painting','art_painting_cartoon',  'art_painting_sketch', 'art_painting_photo', ]
-for file in files: #遍历文件夹
-    if file=='original_record':
-        continue
-    file_words = file.split('_')
-    domain_info = file_words[0][0] + file_words[1][0]
-    ws[chr(ord('A') + column) + str(1)] = domain_info
-    print(domain_info, file_words)
-    file_read = open(path+file, 'r')
-    row = 1
-    for line in file_read:
-        words = line.split(' ')
-        if words[0]=='Accuracy':
-            row += 1
-            ws[chr(ord('A') + column) + str(row)] = words[11].split(':')[1][:-1]
-    column += 1
-print(file_name)
+
+row = 1
+for parameter_pair_path in path_of_different_parameter_pairs:
+    path_of_different_domain_pairs= os.listdir(path + parameter_pair_path)
+    path_of_different_domain_pairs.sort()
+    # files = ['cartoon_art_painting','art_painting_cartoon',  'art_painting_sketch', 'art_painting_photo', ]
+    column = 0
+    ws[chr(ord('A') + 1) + str(row)] = parameter_pair_path
+    row += 1
+    for domain_pair in path_of_different_domain_pairs: #遍历文件夹
+        if domain_pair=='original_record':
+            continue
+        domain_pair_words = domain_pair.split('_')
+        abbreviation = domain_pair_words[0][0] + domain_pair_words[1][0]
+        ws[chr(ord('A') + column) + str(row)] = abbreviation
+        row += 1
+        print(abbreviation, domain_pair_words)
+        lines = open(path + parameter_pair_path + '/' + domain_pair, 'r')
+        for line in lines:
+            words = line.split(' ')
+            if words[0]=='Accuracy':
+                ws[chr(ord('A') + column) + str(row)] = words[11].split(':')[1][:-1]
+                row += 1
+        row -= 4
+        column += 1
+
+    row += 6
+
+    print(parameter_pair_path)
+print(path)
 wb.save("./result.xlsx")
